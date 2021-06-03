698ee3d2498e4f8cb2812549f03c3ac83ce95b50
vishwasmehra <vishwasdeepakmehra09@gmail.com>
2021-06-03 13:10:53 +0530

    Added post method

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
+from _typeshed import IdentityFunction
 import sqlite3
-from flask import Flask, app, request
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import jwt, jwt_required
+
 
 app = Flask(__name__)
 api = Api(app)
 
+items = [
+
+]
+
 class Item(Resource):
+    @jwt_required
     def get(self, name):
-        return{'item': name}, 200
+        for item in items:
+            if item['name'] == name:
+                return{'name': name, 'price': item['price']}, 200
+        return{'message': 'Item not found'}, 404
 
     def post(self, name):
-        pass
+        for item in items:
+            if item['name'] == name:
+                return{'message': 'Item already exists'}, 400
+            
+        data = request.get_json()
+        item = {'name': name, 'price':data['price']}
+        items.append(item)
+        return item, 200
+
+
 
     def put(self, name):
         pass
@@ -20,7 +40,7 @@ class Item(Resource):
 
 class Items(Resource):
     def get(self):
-        pass
+        return{'items': items}
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Items, '/items')
